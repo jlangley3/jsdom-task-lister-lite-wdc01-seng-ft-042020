77ea1cd2c92d202a1bd046760f1a845749d8c9a9
jlangley3 <jlangleyiii@aol.com>
2020-06-04 13:07:55 -0400

    done button still working

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ function handleSubmit(event) {
     // prevent Reloading
 
     event.preventDefault()
-    
+
     // create basic nodes that will be used
 
     let li = document.createElement("li")
     let deleteBtn = document.createElement("button");
-    let editBtn =  document.createElement("button");
-
+    let editBtn = document.createElement("button");
+    // let doneBtn = document.createElement("button");
     // find task list element
 
     let ul = document.querySelector("#tasks")
@@ -26,33 +26,36 @@ function handleSubmit(event) {
     li.innerHTML = document.querySelector("#new-task-description").value
     deleteBtn.innerText = "X"
     editBtn.innerText = "Edit"
+        // doneBtn.innerText = "Done"
 
 
     li.appendChild(editBtn);
     li.appendChild(deleteBtn);
+    // li.appendChild(doneBtn);
 
     deleteBtn.addEventListener("click", handleDelete)
     editBtn.addEventListener("click", handleEdit)
+    li.addEventListener("click", handleDone)
 
 
     ul.appendChild(li)
-
+    event.target.reset()
 }
 
-function handleDelete(event){
+function handleDelete(event) {
     // debugger;
     event.target.parentElement.remove();
 }
 
 function handleEdit(event) {
-    debugger;
+    // debugger;
 
     // creating nodes
 
     let form = document.createElement('form');
     let inputText = document.createElement('input')
     let submitBtn = document.createElement('input')
-    let li =  event.target.parentElement;
+    let li = event.target.parentElement;
 
     // add attributes
 
@@ -74,4 +77,25 @@ function handleEdit(event) {
 
     li.innerHTML = "";
     li.appendChild(form);
+    form.addEventListener("submit", editSubmit)
+
+}
+
+function editSubmit(event) {
+    event.preventDefault()
+    let li = event.target.parentElement
+    li.innerHTML = event.target.children[0].value
+    event.target.remove()
+}
+
+function handleDone(event) {
+    if (event.target.classList.contains("done")) {
+        event.target.classname = "";
+        event.target.style.textDecoration = "none"
+    } else {
+        event.target.style.textDecoration = "line-through";
+        event.target.classname = "done";
+    }
+
+
 }
