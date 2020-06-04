c772f8e79b5810dda6d9aa2a14b225710b273010
jlangley3 <jlangleyiii@aol.com>
2020-06-04 11:37:59 -0400

    base done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    let form = document.querySelector("#create-task-form")
+    form.addEventListener("submit", handleSubmit)
 });
+
+function handleSubmit(event) {
+    event.preventDefault()
+    console.log(event)
+    let li = document.createElement("li")
+    let ul = document.querySelector("#tasks")
+    li.innerHTML = document.querySelector("#new-task-description").value
+    ul.appendChild(li)
+
+}
